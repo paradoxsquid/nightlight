6b9c84914a2b3a060a91f74227f21d463248149f
paradoxsquid <70217602+paradoxsquid@users.noreply.github.com>
2020-08-25 17:09:57 +0100

    ready to package

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 let brightness = 0
 let state = ""
 let strip = neopixel.create(DigitalPin.P8, 12, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) >= 1) {
         state = "ru"
@@ -9,18 +8,25 @@ basic.forever(function () {
         state = "rd"
     }
     if ("ru" == state) {
-        brightness += 20
+        brightness += 5
         if (brightness >= 255) {
             brightness = 255
             state = "wait"
         }
     } else if ("rd" == state) {
-        brightness += -10
+        brightness += -1
         if (brightness <= 0) {
             brightness = 0
             state = "wait"
         }
     }
+    strip.setBrightness(brightness)
+    for (let index = 0; index <= 6; index++) {
+        strip.setPixelColor(index * 2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(index * 2 + 1, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    }
     serial.writeNumber(brightness)
+    serial.writeString("   ")
     serial.writeLine(state)
 })
